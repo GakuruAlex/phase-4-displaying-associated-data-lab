654cb11cbbdf9a332b62147f6af00c732f57558a
alexgicimo <gakurualexgicimo@gmail.com>
2022-11-28 15:20:35 +0300

    Show action for user including Items associated with user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
     def show
         user = User.find_by(id: params[:id] )
         
-        render json: user
+        render json: user , include: :items
     end
 
 
     private
-    
+
     def render_not_found_response
         render json: { error: "User Not Found" }, status: :not_found
     end
