3f143741090e9afd91971bb3f1e57b74ec72d158
alexgicimo <gakurualexgicimo@gmail.com>
2022-11-28 15:18:53 +0300

    Show action for user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def show
+        user = User.find_by(id: params[:id] )
+        
+        render json: user
+    end
+
+
+    private
+    
+    def render_not_found_response
+        render json: { error: "User Not Found" }, status: :not_found
+    end
 end
